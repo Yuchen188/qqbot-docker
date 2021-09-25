e789d2d3165e7eb92f2fa232412edeaacf0f5006
hmq <394047464@qq.com>
2021-09-25 18:56:22 +0800

    update

---
+++ Docker/start.sh
@@ -0,0 +1,1 @@
+cd ../linux-x64 && chmod 777 QQBot.Web && nohup ./QQBot.Web &
--- update.sh
+++
@@ -1,5 +0,0 @@
-kill -9 $( ps -e|grep QQBot.Web |awk '{print $1}') 
-
-git checkout .
-git pull
-chmod 777 QQBot.Web && nohup ./QQBot.Web &
